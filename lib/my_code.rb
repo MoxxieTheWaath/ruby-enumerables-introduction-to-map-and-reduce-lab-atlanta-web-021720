4ef0b906c41e6beeefd0cc2926c50ca025ce758e
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-18 01:05:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,8 @@
 # My Code here....
 def map_to_negativize(source_array)
-  print source_array
+  index = 0
+  while index < source_array.length do
+    source_array[index] = source_array[index] * -1
+    index += 1
+  end
 end
