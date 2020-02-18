ce381675c23c950f13e99e98d9902511435d2c1f
Michael McDonald <mchlmcdnld7@gmail.com>
2020-02-18 01:50:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,3 +36,7 @@ def reduce_to_total(source_array, starting_point = 0)
   total
 end
 def reduce_to_all_true(source_array)
+  index = 0
+  while index < source_array.length do
+    if source_array[index]
+  
